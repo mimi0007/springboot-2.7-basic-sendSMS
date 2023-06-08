82ed466975e5da56aa28fe4545f6db1ac9e328c9
mimi0007 <afsanaaziz.mimi@gmail.com>
2023-06-08 18:04:29 +0600

    Initial Commit

---
+++ src/test/java/com/example/springbootsendotptwilio/SpringbootSendOtpTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsendotptwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSendOtpTwilioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
